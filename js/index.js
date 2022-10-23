7190cfcaa19fbb149014eca54aab023fbc628a92
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2022-10-23 17:55:15 +0200

    no time left to finish this lab, I have to work on my own game project

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
-
-  function startGame() {}
 };
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const backImg = new Image();
+backImg.src = "./images/road.png";
+x = 0,
+y = 0;
+
+setInterval(() => {
+const player = new Image();
+player.src = "./images/car.png";
+player.onload = function () {
+  ctx.drawImage(player, 223, 550, 50, 100);
+};
+  if (y >= canvas.height) {
+    y = 0;
+  } else {
+    y += 10;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(backImg, x, y, canvas.width, canvas.height);
+  ctx.drawImage(backImg, x, y - canvas.height, canvas.width, canvas.height);
+}, 50);
+
